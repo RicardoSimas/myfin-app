9ec8d2132a32e7491b0efcfb83da7c1d733fb02d
Ricardo Simas <rsimas@unigranrio.br>
2021-07-22 17:45:49 -0300

    Add componente Navbar e nevegacao entre views atraves dos componentes.

--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import {withRouter} from 'react-router-dom'
+
 import Card from '../components/card'
 import FormGroup from '../components/form-group'
 
-class CadastroUsuario extends React.Component{
+class CadastroUsuario extends React.Component {
 
     state = {
         nome: '',
@@ -15,64 +17,66 @@ class CadastroUsuario extends React.Component{
         console.log(this.state);
     }
 
-    render(){
-        return(
-            <div className="container">
-                <div className="card mb-3">
-                    <Card title="Cadastro de Usuario">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <div className="bs-component">
-                                    <fieldset>
-                                        <FormGroup label="Nome: *" htmlFor="inputName1">
-                                            <input type="text"
-                                                   id="inputName1"
-                                                   className="form-control"
-                                                   name="nome"                                                
-                                                   onChange={e => this.setState({nome: e.target.value})}
-                                                   placeholder="Digite o nome" >
-                                            </input>
-                                        </FormGroup>
-                                        <FormGroup label="Email: *" htmlFor="inputEmail1">
-                                            <input type="email" 
-                                                   className="form-control" 
-                                                   id="inputEmail1"
-                                                   name="email"
-                                                   onChange={e => this.setState({email: e.target.value})} 
-                                                   aria-describedby="emailHelp" 
-                                                   placeholder="Digite o email" >
-                                            </input>
-                                        </FormGroup>
-                                        <FormGroup label="Senha: *" htmlFor="inputPassword1">
-                                            <input type="password" 
-                                                   className="form-control" 
-                                                   id="inputPassword1"
-                                                   name="senha"
-                                                   onChange={e => this.setState({senha: e.target.value})} 
-                                                   placeholder="Digite a senha" >
-                                            </input>
-                                        </FormGroup>
-                                        <FormGroup label="Repita a Senha: *" htmlFor="inputPassword2">
-                                            <input type="password" 
-                                                   className="form-control" 
-                                                   id="inputPassword2"
-                                                   name="repitaSenha"
-                                                   onChange={e => this.setState({repitaSenha: e.target.value})} 
-                                                   placeholder="Digite a senha" >
-                                            </input>
-                                        </FormGroup>
-                                        <br/>
-                                        <button onClick={this.cadastrar} className="btn btn-success" type="button">Salvar</button>
-                                        <button className="btn btn-danger" type="button">Voltar</button>
-                                    </fieldset>
-                                </div>
+    cancelar = () => {
+        this.props.history.push('/login');
+    }
+
+    render() {
+        return (
+            <div className="card mb-3">
+                <Card title="Cadastro de Usuario">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="bs-component">
+                                <fieldset>
+                                    <FormGroup label="Nome: *" htmlFor="inputName1">
+                                        <input type="text"
+                                            id="inputName1"
+                                            className="form-control"
+                                            name="nome"
+                                            onChange={e => this.setState({ nome: e.target.value })}
+                                            placeholder="Digite o nome" >
+                                        </input>
+                                    </FormGroup>
+                                    <FormGroup label="Email: *" htmlFor="inputEmail1">
+                                        <input type="email"
+                                            className="form-control"
+                                            id="inputEmail1"
+                                            name="email"
+                                            onChange={e => this.setState({ email: e.target.value })}
+                                            aria-describedby="emailHelp"
+                                            placeholder="Digite o email" >
+                                        </input>
+                                    </FormGroup>
+                                    <FormGroup label="Senha: *" htmlFor="inputPassword1">
+                                        <input type="password"
+                                            className="form-control"
+                                            id="inputPassword1"
+                                            name="senha"
+                                            onChange={e => this.setState({ senha: e.target.value })}
+                                            placeholder="Digite a senha" >
+                                        </input>
+                                    </FormGroup>
+                                    <FormGroup label="Repita a Senha: *" htmlFor="inputPassword2">
+                                        <input type="password"
+                                            className="form-control"
+                                            id="inputPassword2"
+                                            name="repitaSenha"
+                                            onChange={e => this.setState({ repitaSenha: e.target.value })}
+                                            placeholder="Digite a senha" >
+                                        </input>
+                                    </FormGroup>
+                                    <br />
+                                    <button onClick={this.cadastrar} className="btn btn-success" type="button">Salvar</button>
+                                    <button onClick={this.cancelar} className="btn btn-danger" type="button">Cancelar</button>
+                                </fieldset>
                             </div>
                         </div>
-                    </Card>
-                </div>
+                    </div>
+                </Card>
             </div>
         )
     }
 }
 
-export default CadastroUsuario
+export default withRouter(CadastroUsuario)
--- src/views/login.js
+++ src/views/login.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Card from '../components/card'
 import FormGroup from '../components/form-group';
 
-class Login extends React.Component{
+import {withRouter} from 'react-router-dom'
+
+class Login extends React.Component {
 
     state = {
         email: '',
@@ -14,51 +16,52 @@ class Login extends React.Component{
         console.log('Senha: ', this.state.senha)
     }
 
-    render(){
-        return(
+    prepareCadastrar = () => {
+        this.props.history.push('/cadastro-usuarios')
+    }
 
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6" style={ {position: 'relative', left: '300px'} }>
-                        <div className="bs-docs-section">
-                            <Card title="Login">
-                                <div className="row">
-                                    <div className="col-lg-12">
-                                        <div className="bs-component">
-                                            <fieldset>
-                                                <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
-                                                    <input type="email"
-                                                        value={this.state.email}
-                                                        onChange={e => this.setState({email: e.target.value})} 
-                                                        className="form-control" 
-                                                        id="exampleInputEmail1" 
-                                                        aria-describedby="emailHelp" 
-                                                        placeholder="Digite o Email"> 
-                                                    </input>
-                                                </FormGroup>
-                                                <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
-                                                    <input type="password"
-                                                        value={this.state.senha}
-                                                        onChange={ e => this.setState({senha: e.target.value})} 
-                                                        className="form-control" 
-                                                        id="exampleInputPassword1" 
-                                                        placeholder="Digite a Senha"> 
-                                                    </input>
-                                                </FormGroup>
-                                                <button onClick={this.entrar} className="btn btn-success">Entrar</button>
-                                                <button className="btn btn-danger">Cadastrar</button>
-                                            </fieldset>
-                                        </div>
+    render() {
+        return (
+            <div className="row">
+                <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
+                    <div className="bs-docs-section">
+                        <Card title="Login">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="bs-component">
+                                        <fieldset>
+                                            <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
+                                                <input type="email"
+                                                    value={this.state.email}
+                                                    onChange={e => this.setState({ email: e.target.value })}
+                                                    className="form-control"
+                                                    id="exampleInputEmail1"
+                                                    aria-describedby="emailHelp"
+                                                    placeholder="Digite o Email">
+                                                </input>
+                                            </FormGroup>
+                                            <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
+                                                <input type="password"
+                                                    value={this.state.senha}
+                                                    onChange={e => this.setState({ senha: e.target.value })}
+                                                    className="form-control"
+                                                    id="exampleInputPassword1"
+                                                    placeholder="Digite a Senha">
+                                                </input>
+                                            </FormGroup>
+                                            <br/>
+                                            <button onClick={this.entrar} className="btn btn-success">Entrar</button>
+                                            <button onClick={this.prepareCadastrar} className="btn btn-danger">Cadastrar</button>
+                                        </fieldset>
                                     </div>
                                 </div>
-                            </Card>
-                        </div>
-                    </div>  
+                            </div>
+                        </Card>
+                    </div>
                 </div>
             </div>
-
         )
     }
 }
 
-export default Login;
+export default withRouter(Login) 
